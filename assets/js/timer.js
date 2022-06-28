e9ac9fb3d267ae32b3f7328abd69d3f8f30f46e1
memo <memorstx@gmail.com>
2022-06-28 12:22:36 -0500

    :bookmark: Función para crear tarjetas de eventos de carreras de caracoles

---
+++ assets/js/timer.js
@@ -0,0 +1,15 @@
+
+// countdown
+
+// function countdown(element, seconds) {
+//     var interval = setInterval(function() {
+//         // element.innerHTML = seconds;
+//         if (--seconds < 0) {
+//             clearInterval(interval);
+//         }
+//     }, 1000);
+//     return interval;
+// }
+
+
+
